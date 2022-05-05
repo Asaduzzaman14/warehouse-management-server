edc359bf236ef768afd84fe92279f638774a1fc7
Md Asaduzaman <asadasad9282@gmail.com>
2022-05-05 17:06:57 +0600

    create item api end point

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');;
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
+
 const app = express()
-
-
 const port = process.env.PORT || 5000
 
 // modelwear
@@ -12,6 +12,21 @@ app.use(express.json())
 
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dci89.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+
+        await client.connect()
+        const itemCollaction = client.db('warehouseMenagement').collection('items')
+
+        app.get('/item', async (req, res) => {
+            const query = {}
+            const cursor = itemCollaction.find(query)
+            const items = await cursor.toArray()
+            res.send(items)
+        })
 
 
 
@@ -22,6 +37,14 @@ app.use(express.json())
 
 
 
+    } finally {
+
+    }
+
+
+}
+
+run().catch(console.dir)
 
 
 
